f9b76db723333be5bdf2d5879d52c368c2507821
Jobs-rw <mjabush@yahoo.fr>
2023-03-15 12:14:30 -0700

    upda

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+#include "holberton.h"
 /**
- * main -add positive numbers
- * @argc: int
- * @argv: char *
- * Return: always 0
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-int i;
-int sum = 0;
+	int i, sum = 0;
 
-if (argc > 1)
-{
-	for (i = 1; i <= (argc - 1); i++)
+	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
 	{
-		sum = sum + atoi(argv[i]);
-
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
-}
 
-if (argc == 1)
-	printf("0\n");
-
-return (0);
-
+	return (0);
 }
