39ad010d9ba05fad339dba68e8b8a7824a3fe0fd
Jobs-rw <mjabush@yahoo.fr>
2023-04-10 02:32:49 -0700

    debug5

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+unsigned int binary_to_uint(const char *b) {
+    unsigned int result = 0;
+    int i;
+
+    if (b == NULL) {
+        return 0;
+    }
+
+    for (i = 0; b[i] != '\0'; i++) {
+        if (b[i] == '0') {
+            result <<= 1; // equivalent to result = result * 2
+        } else if (b[i] == '1') {
+            result <<= 1;
+            result |= 1; // equivalent to result = result * 2 + 1
+        } else {
+            return 0;
+        }
+    }
+
+    return result;
+}
