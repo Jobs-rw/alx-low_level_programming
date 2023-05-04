08938919b1050ea830e10d1d52bf16552fc96425
Jobs-rw <mjabush@yahoo.fr>
2023-05-04 06:37:40 -0700

    commit on 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,33 @@
 #include "main.h"
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- *
- * Return: the converted number, or 0 if there is one or more chars in the
- * string b that is not 0 or 1 or b is NULL
- */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int result = 0;
-	int i = 0;
 
-	if (b == NULL)
+/**
+ * binary_to_uint - converts a binary string to an unsigned int.
+ * @binary_string: pointer to a string of 0 and 1 chars.
+ *
+ * Return: the converted number, or 0 if there is one
+ * or more chars in the string
+ * that is not 0 or 1 or if b is NULL.
+ */
+unsigned int binary_to_uint(const char *binary_string)
+{
+	unsigned int ihene = 0;
+
+	if (!binary_string)
 		return (0);
 
-	while (b[i] != '\0')
+	while (*binary_string != '\0')
 	{
-		if (b[i] == '0' || b[i] == '1')
+		if (*binary_string == '0' || *binary_string == '1')
 		{
-			result = result << 1;
-			if (b[i] == '1')
-				result |= 1;
-			i++;
+			ihene <<= 1;
+			ihene += (*binary_string - '0');
+			binary_string++;
 		}
 		else
+		{
 			return (0);
+		}
 	}
 
-	return (result);
+	return (ihene);
 }
